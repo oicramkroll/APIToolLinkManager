fb743060414eebfc97765de39c1e49cca16c9bee
Márcio De Andrade Sena <marcio.as@outlook.com>
2020-06-10 15:48:42 -0300

    expose header

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const cors = require('cors');
 const {errors} = require('celebrate');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:'*',
+    exposedHeaders:'*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(require('./routes'));
 app.use(errors());
+
 app.listen(process.env.PORT || 3333);
